e550265fda79b670f0f7c2a2d17fbf303a908a7e
Christof Schuetzenhoefer <christof.schuetzenhoefer@student.tugraz.at>
2021-07-02 18:33:52 +0200

    initial version for the project

---
+++ expert_system/src/main/java/com/IntelligentSystem/MainExpertSystem.java
@@ -0,0 +1,94 @@
+package com.IntelligentSystem;
+
+import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.ObjectProperty;
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.util.iterator.ExtendedIterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainExpertSystem {
+
+    private static final Logger log = LoggerFactory.getLogger(MainExpertSystem.class);
+
+    /**
+     * An SPARQL example
+     */
+    private static final String query = String.join(System.lineSeparator(),
+            "                PREFIX knowrob: <http://ias.cs.tum.edu/kb/knowrob.owl#>",
+            "                PREFIX comp:   <http://ias.cs.tum.edu/kb/srdl2-comp.owl#>",
+            "                PREFIX owl:                      <http://www.w3.org/2002/07/owl#>",
+            "                PREFIX rdfs:      <http://www.w3.org/2000/01/rdf-schema#>",
+            "                                                                                                                                       ",
+            "                SELECT ?x                                                                                                   ",
+            "                WHERE {                                                                                                     ",
+            "                {                                                                                                                     ",
+            "                  ?x rdfs:subClassOf [                                                                             ",
+            "                    a owl:Restriction;                                                                                ",
+            "                    owl:onProperty ?p ;                                                                            ",
+            "                    owl:someValuesFrom ?y                                                                   ",
+            "                  ] .                                                                                                                 ",
+            "                }}");
+
+    private static final String document = "http://ias.cs.tum.edu/kb/knowrob.owl#";
+
+    /**
+     * Examples of SPARQL with apache jena
+     *
+     * @param args none
+     */
+    public static void main(String[] args) {
+
+        log.info("Example SPARQL query: ");
+        log.info(query);
+
+        log.info("Creates the OWL model");
+        OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
+
+        log.info("Reads the document");
+        model.read(document);
+
+        log.info("Query execution is created for the example query");
+        QueryExecution qexec = QueryExecutionFactory.create(query, model);
+
+        log.info("Obtains the result set");
+        ResultSet results = qexec.execSelect();
+
+        log.info("Iterates over the result set");
+        while (results.hasNext()) {
+            QuerySolution sol = results.nextSolution();
+            log.info("Solution: " + sol);
+        }
+
+        log.info("Obtain the properties of the model");
+        ExtendedIterator<ObjectProperty> properties = model.listObjectProperties();
+
+		/*log.info("Iterates over the properties");
+		while (properties.hasNext()) {
+			log.info("Property: " + properties.next().getLocalName());
+		}
+
+		log.info("Obtains an iterator over individual resources");
+		ExtendedIterator<Individual> individualResources = model.listIndividuals();
+
+		log.info("Iterates over the resources");
+		while (individualResources.hasNext()) {
+			log.info("Individual resource: " + individualResources.next());
+		}
+
+		log.info("Obtains an extended iterator over classes");
+		ExtendedIterator<OntClass> classes = model.listClasses();
+
+		log.info("Iterates over the classes");
+		while (classes.hasNext()) {
+			log.info("Class: " + classes.next().toString());
+		}*/
+    }
+}
